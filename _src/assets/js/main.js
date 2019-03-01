83350bc02bc8cd1c4d64f3320199756a14822fc4
Katherine Briceño <kath.gbl@gmail.com>
2019-03-01 13:33:43 +0100

    Fix js errors

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-console.log('>> Ready :)');
+const buttonGo = document.querySelector('.btn__main');
+let input = document.querySelector('#input');
+let clueForUser = document.querySelector('.clue');
+let randomNumber = getRandomNumber(100);
+const counterAttempts = document.querySelector('.counter__main');
+
+//Function to generate random numbers
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+//Counter
+let counter = 0;
+counterAttempts.innerHTML = counter;
+
+//Function to give clues to the user
+function chosenNumber() {
+  let inputUser = parseInt(input.value);
+
+  if (inputUser < randomNumber) {
+    clueForUser.innerHTML = 'too low, try again!';
+    counter += 1;
+    counterAttempts.innerHTML = counter;
+  }
+  else if (inputUser > randomNumber) {
+    clueForUser.innerHTML = 'too high, try again!';
+    counter += 1;
+    counterAttempts.innerHTML = counter;
+  }
+  else {
+    clueForUser.innerHTML = 'You won... Yay!';
+  }
+}
+
+buttonGo.addEventListener('click', chosenNumber);
